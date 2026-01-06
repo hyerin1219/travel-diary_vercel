a727efcad90c4a5e339d48ed1a9493b5a152cc9c
hyerin <gpfls662@gmail.com>
2026-01-06 17:32:38 +0900

    fix: 빌드 오류 수정

--- src/lib/firebase/firebaseApp.ts
+++ src/lib/firebase/firebaseApp.ts
@@ -1,20 +1,25 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
 };
 
-// 중복 초기화 + 빌드 안정성
-export const firebaseApp = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(firebaseApp);
 
-export const auth = getAuth(firebaseApp);
-export const googleProvider = new GoogleAuthProvider();
+// Firebase 인증 인스턴스 가져오기
+const auth = getAuth(firebaseApp);
+const googleProvider = new GoogleAuthProvider();
+export { auth, googleProvider };
