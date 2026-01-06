0b95776f9461e5a3ebc3ec1c004e916728b03b1d
hyerin <gpfls662@gmail.com>
2026-01-06 16:53:33 +0900

    fix: firebase환경변수 이름 수정

--- src/lib/firebase/firebaseApp.ts
+++ src/lib/firebase/firebaseApp.ts
@@ -1,25 +1,20 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDERID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
-// Initialize Cloud Firestore and get a reference to the service
+// 중복 초기화 + 빌드 안정성
+export const firebaseApp = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+
 export const db = getFirestore(firebaseApp);
 
-// Firebase 인증 인스턴스 가져오기
-const auth = getAuth(firebaseApp);
-const googleProvider = new GoogleAuthProvider();
-export { auth, googleProvider };
+export const auth = getAuth(firebaseApp);
+export const googleProvider = new GoogleAuthProvider();
